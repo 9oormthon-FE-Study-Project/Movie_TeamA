9264ad57fdd38265d8651f6ec2440063a736ab1a
lnylnylnylny <lnylnylnylny@naver.com>
2025-05-29 14:57:45 +0900

    fix: tsx,ts 오류 수정

--- movie_project/src/utils/renderStars.tsx
+++ movie_project/src/utils/renderStars.tsx
@@ -1,4 +1,4 @@
-import star from '../../assets/icon/star.svg';
+import star from '../assets/icon/star.svg';
 
 export const renderStars = (vote: number) => {
   const starCount = Math.round(vote / 2);
