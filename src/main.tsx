202efda87310e47d3a4be9cfb7260e6859ffb663
leeseoleeseoyeon <seoyeon6468@gmail.com>
2025-06-06 00:20:59 +0900

    refactor : 폴더 이름 수정(소문자)

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react';
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
-import Home from './Pages/Home/Home.tsx'
-import Login from './Pages/Login/Login.tsx'
-import Signup from './Pages/Signup/Signup.tsx'
+import Home from './pages/home/Home.tsx'
+import Login from './pages/login/Login.tsx'
+import Signup from './pages/signup/Signup.tsx'
 
 const router = createBrowserRouter([
   {
