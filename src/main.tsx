cd64ec2ee4e01c5a02de94c2c92267486641eb3b
lnylnylnylny <lnylnylnylny@naver.com>
2025-06-06 00:44:22 +0900

    refactor : 파일구조 정리

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,33 @@
-
 import React from 'react';
-import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+import App from './App.tsx';
 import Home from './pages/Home.tsx';
-import Login from './Pages/Login/Login.tsx'
-import Signup from './Pages/Signup/Signup.tsx'
+import Login from './pages/Login.tsx';
+import Signup from './pages/Signup.tsx';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <App />,
     children: [
       {
-        index : true,
+        index: true,
         element: <Home />,
       },
       {
-        path :"login",
+        path: 'login',
         element: <Login />,
       },
       {
-        path: "signup",
+        path: 'signup',
         element: <Signup />,
       },
-    ]
-  }
-])
+    ],
+  },
+]);
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <RouterProvider router={router} />
 );
