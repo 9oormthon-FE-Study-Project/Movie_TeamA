4384ede604846ddcf4d1d570f99f04ec59b38f2d
lnylnylnylny <lnylnylnylny@naver.com>
2025-06-09 16:48:09 +0900

    feat: 로그인 + 회원가입 화면에도 네비게이션바 추가

--- src/components/home/Nav.tsx
+++ src/components/home/Nav.tsx
@@ -1,10 +1,14 @@
 import { CiSearch } from 'react-icons/ci';
 import { IoCloseOutline, IoHomeOutline } from 'react-icons/io5';
 import { FaUserCircle } from 'react-icons/fa';
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 const Nav = () => {
   const navigate = useNavigate();
+  const location = useLocation();
+
+  const isAuthPage =
+    location.pathname === '/login' || location.pathname === '/signup';
 
   return (
     <div className='mb-2 flex w-full items-center justify-center bg-black px-4 py-2'>
@@ -13,17 +17,19 @@ const Nav = () => {
           <IoHomeOutline onClick={() => navigate('/')} />
         </div>
 
-        <div className='flex flex-1 items-center rounded-full bg-neutral-900 px-4 py-2'>
-          <input
-            type='text'
-            placeholder='검색어를 입력하세요...'
-            className='flex-1 bg-transparent text-white placeholder-gray-400 focus:outline-none'
-          />
-          <div className='ml-2 flex cursor-pointer items-center gap-3 text-2xl text-white'>
-            <IoCloseOutline />
-            <CiSearch />
+        {!isAuthPage && (
+          <div className='flex flex-1 items-center rounded-full bg-neutral-900 px-4 py-2'>
+            <input
+              type='text'
+              placeholder='검색어를 입력하세요...'
+              className='flex-1 bg-transparent text-white placeholder-gray-400 focus:outline-none'
+            />
+            <div className='ml-2 flex cursor-pointer items-center gap-3 text-2xl text-white'>
+              <IoCloseOutline />
+              <CiSearch />
+            </div>
           </div>
-        </div>
+        )}
 
         <div className='cursor-pointer text-2xl text-white'>
           <FaUserCircle onClick={() => navigate('/login')} />
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,6 +3,7 @@ import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { useAuthStore } from '../store/authStore';
 import { LoginForm } from '../types/login';
+import Nav from '../components/home/Nav';
 
 const Login = () => {
   const {
@@ -44,59 +45,64 @@ const Login = () => {
   }
 
   return (
-    <div className='flex min-h-screen items-center justify-center bg-black'>
-      <div className='w-full max-w-md rounded-lg bg-black p-8 shadow-md'>
-        <h2 className='mb-6 text-center text-2xl font-bold text-white'>
-          로그인
-        </h2>
-        <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
-          <div>
-            <input
-              type='text'
-              {...register('username', { required: '아이디를 입력하세요.' })}
-              placeholder='아이디'
-              className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
-            />
-            {errors.username && (
-              <p className='mt-1 text-sm text-red-400'>
-                {errors.username.message}
-              </p>
-            )}
-          </div>
-          <div>
-            <input
-              type='password'
-              {...register('password', { required: '비밀번호를 입력하세요.' })}
-              placeholder='비밀번호'
-              className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
-              onKeyDown={(e) => {
-                if (e.key === ' ') {
-                  e.preventDefault(); // 공백 입력 방지
-                }
-              }}
-            />
-            {errors.password && (
-              <p className='mt-1 text-sm text-red-400'>
-                {errors.password.message}
-              </p>
-            )}
-          </div>
-          <button
-            type='submit'
-            className='w-full cursor-pointer rounded bg-red-800 py-2 text-white transition hover:bg-gray-700'
-          >
+    <div className='min-h-screen bg-black'>
+      <Nav />
+      <div className='flex min-h-[calc(100vh-150px)] items-center justify-center'>
+        <div className='w-full max-w-md rounded-lg bg-black p-8 shadow-md'>
+          <h2 className='mb-6 text-center text-2xl font-bold text-white'>
             로그인
-          </button>
-        </form>
-        <p className='mt-4 text-center text-gray-400'>
-          계정이 없으신가요?{' '}
-          <button
-            className='cursor-pointer text-gray-300 underline'
-            onClick={() => navigate('/signup')}
-          >
-            회원가입
-          </button>
-        </p>
+          </h2>
+          <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
+            <div>
+              <input
+                type='text'
+                {...register('username', { required: '아이디를 입력하세요.' })}
+                placeholder='아이디'
+                className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
+              />
+              {errors.username && (
+                <p className='mt-1 text-sm text-red-400'>
+                  {errors.username.message}
+                </p>
+              )}
+            </div>
+            <div>
+              <input
+                type='password'
+                {...register('password', {
+                  required: '비밀번호를 입력하세요.',
+                })}
+                placeholder='비밀번호'
+                className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
+                onKeyDown={(e) => {
+                  if (e.key === ' ') {
+                    e.preventDefault(); // 공백 입력 방지
+                  }
+                }}
+              />
+              {errors.password && (
+                <p className='mt-1 text-sm text-red-400'>
+                  {errors.password.message}
+                </p>
+              )}
+            </div>
+            <button
+              type='submit'
+              className='w-full cursor-pointer rounded bg-red-800 py-2 text-white transition hover:bg-gray-700'
+            >
+              로그인
+            </button>
+          </form>
+          <p className='mt-4 text-center text-gray-400'>
+            계정이 없으신가요?{' '}
+            <button
+              className='cursor-pointer text-gray-300 underline'
+              onClick={() => navigate('/signup')}
+            >
+              회원가입
+            </button>
+          </p>
+        </div>
       </div>
     </div>
   );
