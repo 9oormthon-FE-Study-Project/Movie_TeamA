38be0aa44e45105fbfa6f25526f9e6e7bbfc6ba3
lnylnylnylny <lnylnylnylny@naver.com>
2025-06-09 16:31:57 +0900

    feat: css 추가

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -5,7 +5,12 @@ import { SignupForm } from '../types/signup';
 // import axios from 'axios';
 
 const Signup = () => {
-  const { register, handleSubmit, formState: { errors }, watch } = useForm<SignupForm>();
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    watch,
+  } = useForm<SignupForm>();
   const navigate = useNavigate();
 
   const onSubmit = async (data: SignupForm) => {
@@ -22,43 +27,54 @@ const Signup = () => {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-black">
-      <div className="bg-black p-8 rounded-lg shadow-md w-full max-w-md">
-        <h2 className="text-2xl font-bold text-white mb-6 text-center">회원가입</h2>
-        <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+    <div className='flex min-h-screen items-center justify-center bg-black'>
+      <div className='w-full max-w-md rounded-lg bg-black p-8 shadow-md'>
+        <h2 className='mb-6 text-center text-2xl font-bold text-white'>
+          회원가입
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)} className='space-y-4'>
           <div>
             <input
-              type="text"
+              type='text'
               {...register('username', { required: '아이디를 입력하세요.' })}
-              placeholder="아이디"
-              className="w-full px-4 py-2 rounded bg-gray-300 text-gray-700 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+              placeholder='아이디'
+              className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
             />
-            {errors.username && <p className="text-red-400 text-sm mt-1">{errors.username.message}</p>}
+            {errors.username && (
+              <p className='mt-1 text-sm text-red-400'>
+                {errors.username.message}
+              </p>
+            )}
           </div>
           <div>
             <input
-              type="password"
+              type='password'
               {...register('password', { required: '비밀번호를 입력하세요.' })}
-              placeholder="비밀번호"
-              className="w-full px-4 py-2 rounded bg-gray-300 text-gray-700 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+              placeholder='비밀번호'
+              className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
               onKeyDown={(e) => {
                 if (e.key === ' ') {
                   e.preventDefault(); // 공백 입력 방지
                 }
               }}
             />
-            {errors.password && <p className="text-red-400 text-sm mt-1">{errors.password.message}</p>}
+            {errors.password && (
+              <p className='mt-1 text-sm text-red-400'>
+                {errors.password.message}
+              </p>
+            )}
           </div>
           <div>
             <input
-              type="password"
+              type='password'
               {...register('confirmPassword', {
                 required: '비밀번호 확인을 입력하세요.',
                 validate: (value) =>
-                  value === watch('password') || '비밀번호가 일치하지 않습니다.',
+                  value === watch('password') ||
+                  '비밀번호가 일치하지 않습니다.',
               })}
-              placeholder="비밀번호 확인"
-              className="w-full px-4 py-2 rounded bg-gray-300 text-gray-700 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+              placeholder='비밀번호 확인'
+              className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
               onKeyDown={(e) => {
                 if (e.key === ' ') {
                   e.preventDefault(); // 공백 입력 방지
@@ -66,42 +82,66 @@ const Signup = () => {
               }}
             />
             {errors.confirmPassword && (
-              <p className="text-red-400 text-sm mt-1">{errors.confirmPassword.message}</p>
+              <p className='mt-1 text-sm text-red-400'>
+                {errors.confirmPassword.message}
+              </p>
             )}
           </div>
           <div>
             <input
-              type="email"
+              type='email'
               {...register('email', { required: '이메일을 입력하세요.' })}
-              placeholder="이메일"
-              className="w-full px-4 py-2 rounded bg-gray-300 text-gray-700 border border-gray-700  focus:outline-none focus:ring-2 focus:ring-gray-500"
+              placeholder='이메일'
+              className='w-full rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
             />
-            {errors.email && <p className="text-red-400 text-sm mt-1">{errors.email.message}</p>}
+            {errors.email && (
+              <p className='mt-1 text-sm text-red-400'>
+                {errors.email.message}
+              </p>
+            )}
           </div>
           <div>
             <select
               {...register('gender', { required: '성별을 선택하세요.' })}
-              className="w-full px-4 py-2 rounded bg-gray-300 text-gray-700 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+              className='w-full cursor-pointer rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
             >
-              <option value="">성별 선택</option>
-              <option value="male">남성</option>
-              <option value="female">여성</option>
+              <option value=''>성별 선택</option>
+              <option value='male'>남성</option>
+              <option value='female'>여성</option>
             </select>
-            {errors.gender && <p className="text-red-400 text-sm mt-1">{errors.gender.message}</p>}
+            {errors.gender && (
+              <p className='mt-1 text-sm text-red-400'>
+                {errors.gender.message}
+              </p>
+            )}
           </div>
           <div>
             <input
-              type="date"
+              type='date'
               {...register('birthdate', { required: '생년월일을 입력하세요.' })}
-              className="w-full px-4 py-2 rounded bg-gray-300 text-gray-700 border border-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-500"
+              className='w-full cursor-pointer rounded border border-gray-700 bg-gray-300 px-4 py-2 text-gray-700 focus:ring-2 focus:ring-gray-500 focus:outline-none'
             />
-            {errors.birthdate && <p className="text-red-400 text-sm mt-1">{errors.birthdate.message}</p>}
+            {errors.birthdate && (
+              <p className='mt-1 text-sm text-red-400'>
+                {errors.birthdate.message}
+              </p>
+            )}
           </div>
-          <button type="submit" className="w-full py-2 bg-red-800 hover:bg-gray-700 text-white rounded transition">회원가입</button>
+          <button
+            type='submit'
+            className='w-full cursor-pointer rounded bg-red-800 py-2 text-white transition hover:bg-gray-700'
+          >
+            회원가입
+          </button>
         </form>
-        <p className="text-gray-400 text-center mt-4">
+        <p className='mt-4 text-center text-gray-400'>
           이미 계정이 있으신가요?{' '}
-          <button className="text-gray-300 underline" onClick={() => navigate('/login')}>로그인</button>
+          <button
+            className='cursor-pointer text-gray-300 underline'
+            onClick={() => navigate('/login')}
+          >
+            로그인
+          </button>
         </p>
       </div>
     </div>
